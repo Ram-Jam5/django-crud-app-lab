bc809e77ed5d5e40dc77cf08cc4b322ba1c0661c
Ram-Jam5 <edgeillusive@gmail.com>
2024-08-13 14:33:36 -0500

    add views and template for home and index page.

---
+++ teambuilderapp/main_app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse('<h1>Hello world</h1>')
+
+def team_index(request):
+    return render(request, 'index.html')
